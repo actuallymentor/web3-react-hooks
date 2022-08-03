69007f85c565ce563e86a93c41ee9ca5874aa657
Mentor <mentor@palokaj.co>
2022-08-03 13:53:48 +0200

    📚 JSdoc specs

--- modules/hooks.js
+++ modules/hooks.js
@@ -1,9 +1,12 @@
 import { log, setListenerAndReturnUnlistener } from './helpers'
-import { useState, useEffect, useRef } from "react"
+import { useState, useEffect } from "react"
 import useInterval from 'use-interval'
 
 
-// Connection state (is metamask connected to RPC)
+/**
+* Connection state of the provider
+* @returns {Boolean} whether the provider is connected to it's RPC endpint
+*/
 export function useIsConnected() {
 
 	// Check for initial status
@@ -34,12 +37,16 @@ export function useIsConnected() {
 
 }
 
-// Chain ID
-export function useChainID( defaultChain ) {
+/**
+* The currencly selected chain ID
+* @returns {String} the ID of the currently selected chain 
+*/
+export function useChainID() {
 
 	const isConnected = useIsConnected()
-	const [ chainID, setChainID ] = useState( defaultChain )
+	const [ chainID, setChainID ] = useState()
 
+	// Listen to chain changes
 	useEffect( f => setListenerAndReturnUnlistener( window.ethereum, 'chainChanged', chain_id => {
 		log( `⛓ 🔄 Chain ID changed from ${ chainID } to ${ chain_id }` )
 		setChainID( chain_id )
@@ -74,8 +81,10 @@ export function useChainID( defaultChain ) {
 }
 
 
-
-// Address hook
+/**
+* The last known selected address as reported by the provider
+* @returns {String} The last known address the provider was connected to. This is not a guarantee that the user is currently connected with a specific wallet. Always call ethereum.request({ method: 'eth_accounts' }) before taking actions that require the wallet to be unlocked and connected.
+*/
 export function useAddress() {
 
 	const [ address, setAddress ] = useState( window.ethereum?.selectedAddress )
@@ -120,14 +129,16 @@ export function useAddress() {
 
 }
 
-/* ///////////////////////////////
-// ENS
-// /////////////////////////////*/
+/**
+* The ENS of the current address, as reported by the provider
+* @returns {String} the ENS if it was able to resolve through the provider 
+*/
 export function useENS(  ) {
 
 	const address = useAddress()
 	const [ ENS, setENS ] = useState(  )
 
+	// Resolve ENS on address changed
 	useEffect( (  ) => {
 
 		let cancelled = false;
